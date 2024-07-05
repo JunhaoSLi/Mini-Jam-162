0dab72bf60e1e763043e303ad36fcc2de7e410c4
Junhao Li <junhaoli2019@u.northwestern.edu>
2024-07-05 18:58:59 -0500

    partway through player and controls

--- Game Jam of Heat/Assets/Scripts/PlayerController.cs
+++ Game Jam of Heat/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,4 +23,31 @@ public class NewBehaviourScript : MonoBehaviour
     {
         
     }
+
+    // called every 0.02 seconds
+    private void FixedUpdate()
+    {
+        // If movement input is not 0, try to move
+        if (movementInput != Vector2.zero)
+        {
+            // Check for potential collisions
+            int collisionsCount = rb.Cast(
+                movementInput,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset
+            );
+
+            if (collisionsCount == 0)
+            {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+
 }
